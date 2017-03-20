d01d762ff08cdaef476d728d20a031d6a9ab0147
Logan Place <place1l@cmich.edu>
2017-03-20 08:58:48 -0400

    Adds models for music

--- music/models.py
+++ music/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Track(models.Model):
+    artist = models.CharField(max_length=200)
+    album_title = models.CharField(max_length=200)
+    title = models.CharField(max_length=300)
+    description = models.CharField(max_length=500)
+    #eventually a file will be uploaded for track
+    track = models.CharField(max_length=1000)
+    #eventually a file will be uploaded for track_pic
+    track_pic = models.CharField(max_length=1000)
