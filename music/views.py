82d0f922e17a975d63a801e1c28d23a713f7a830
Logan Place <place1l@cmich.edu>
2017-03-22 12:43:14 -0400

    adds template for music

--- music/views.py
+++ music/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from .models import Track
+from django.template import loader
 
 def index(request):
     all_tracks = Track.objects.all()
-    html = ''
-    for track in all_tracks:
-        url = '/music/' + str(track.id) + '/'
-        html += '<a href="'+ url +'">'+track.track_title+'</a></br>'
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_tracks' : all_tracks,
+    }
+    return HttpResponse(template.render(context, request))
 
+#@TODO: need a template for track -- Track title displayed
 def track(request, track_id):
     return HttpResponse("<h2>track " + str(track_id) + "</h2>")
