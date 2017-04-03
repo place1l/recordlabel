5816849ab2ab0eae4efba0ee1fd900fe2fa631d0
Logan Place <place1l@cmich.edu>
2017-04-03 10:47:17 -0400

    condenses views for music, track method in music now renders correct html ('music/track.html') changes track.html

--- music/views.py
+++ music/views.py
@@ -1,15 +1,16 @@
+from django.http import Http404
 from django.http import HttpResponse
 from .models import Track
-from django.template import loader
+from django.shortcuts import render
 
 def index(request):
     all_tracks = Track.objects.all()
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_tracks' : all_tracks,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', {'all_tracks' : all_tracks})
 
 #@TODO: need a template for track -- Track title displayed
 def track(request, track_id):
-    return HttpResponse("<h2>track " + str(track_id) + "</h2>")
+    try:
+        track = Track.objects.get(pk = track_id)
+    except Track.DoesNotExist:
+        raise Http404("Track Not Found")
+    return render(request, 'music/track.html', {'track' : track})
