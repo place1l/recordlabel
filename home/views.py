c93d82995cd62e09c9fb95ecf0c4dc5f0ea27e93
Logan Place <place1l@cmich.edu>
2017-04-05 12:49:04 -0400

    adds links to Artist/Music/Events on homepage

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h1>HOMEPAGE</h1>\n<a href = /music/>Music</a>")
+    return HttpResponse("<h1>HOMEPAGE</h1><ul><li><a href = /artist/>Artists</a></li><li><a href = /music/>Music</a></li><li><a href = /events/>Events</a></li></ul>")
