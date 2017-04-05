b81c6b22a1b2b0342dbf4d4cd114fee194687416
Logan Place <place1l@cmich.edu>
2017-04-05 13:21:49 -0400

    changes artist models

--- artist/models.py
+++ artist/models.py
@@ -2,11 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Bio(models.Model):
-    artist_name = models.CharField(max_length=100)
-    artist_location = models.CharField(max_length=100)
-    artist_bio = models.CharField(max_length=1000)
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    bio = models.CharField(max_length=1000)
     #eventually a file will be uploaded for artist_picture
-    artist_picture = models.CharField(max_length=1500)
+    picture = models.CharField(max_length=1500)
     #eventually a file will be uploaded for example_tracks
-    example_tracks = models.CharField(max_length=1000)
+    tracks = models.CharField(max_length=1000)
